5f0c1a6a92856b782d8e0ae259f69331dec130c9
Ross Poulton <ross@rossp.org>
2012-03-19 02:52:07 +0000

    Add South migrations - initial migration

---
+++ menu/migrations/0001-initial.py
@@ -0,0 +1,63 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Menu'
+        db.create_table('menu_menu', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
+            ('base_url', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
+            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+        ))
+        db.send_create_signal('menu', ['Menu'])
+
+        # Adding model 'MenuItem'
+        db.create_table('menu_menuitem', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('menu', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['menu.Menu'])),
+            ('order', self.gf('django.db.models.fields.IntegerField')()),
+            ('link_url', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('login_required', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal('menu', ['MenuItem'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Menu'
+        db.delete_table('menu_menu')
+
+        # Deleting model 'MenuItem'
+        db.delete_table('menu_menuitem')
+
+
+    models = {
+        'menu.menu': {
+            'Meta': {'object_name': 'Menu'},
+            'base_url': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        'menu.menuitem': {
+            'Meta': {'object_name': 'MenuItem'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'link_url': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'login_required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'menu': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['menu.Menu']"}),
+            'order': ('django.db.models.fields.IntegerField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['menu']
+
